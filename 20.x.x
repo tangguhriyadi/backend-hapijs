ec3c1c737e6d4aa32bdd2ff25d278c7537c6d0f2
tangguhriyadi <mtangguh97@gmail.com>
2022-12-29 13:13:28 +0700

    2nd

---
+++ 20.x.x
@@ -0,0 +1,19 @@
+yarn add v1.22.19
+[1/4] Resolving packages...
+[2/4] Fetching packages...
+[3/4] Linking dependencies...
+[4/4] Building fresh packages...
+success Saved lockfile.
+success Saved 6 new dependencies.
+info Direct dependencies
+├─ @hapi/hapi@21.1.0
+├─ hapi-swagger@15.0.0
+└─ joi@17.7.0
+info All dependencies
+├─ @hapi/hapi@21.1.0
+├─ @sideway/address@4.1.4
+├─ @sideway/formula@3.0.1
+├─ @sideway/pinpoint@2.0.0
+├─ hapi-swagger@15.0.0
+└─ joi@17.7.0
+Done in 4.86s.
--- src/server.js
+++ src/server.js
@@ -1,65 +1,180 @@
 "use-strict";
 
 const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
-const Inert = require('@hapi/inert')
-const Vision = require('@hapi/vision')
-const Connection = require('../dbconfig')
-const path = require("path")
-
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+const HapiSwagger = require("hapi-swagger");
+const Joi = require("joi");
+const { connect } = require("../dbconfig");
 
 const init = async () => {
   const server = Hapi.server({
     host: "localhost",
     port: 3000,
+    routes: {
+      cors: true,
+    },
   });
 
+  const swaggerOptions = {
+    info: {
+      title: "Test API Docs",
+      version: "123",
+    },
+  };
+
   await server.register([
+    Inert,
+    Vision,
     {
-      plugin: require("hapi-geo-locate"),
-      options: {
-        enabledByDefault: true
-      },
-    },
-    {
-      plugin: Inert
-    },
-    {
-      plugin: Vision
+      plugin: HapiSwagger,
+      options: swaggerOptions,
     },
   ]);
 
-  server.views({
-    engines:{
-        html:require('handlebars')
-    },
-    path: path.join(__dirname,'../views' )
-  })
+  connect
+    .authenticate()
+    .then(() => console.log("connected to Database"))
+    .catch((err) => console.log(err));
 
-  /* server.route(routes); */
   server.route([
     {
-        method:'GET',
-        path:'/getData',
+      method: "GET",
+      path: "/faqs",
+      options: {
         handler: async (req, h) => {
-            const dbConnection = await Connection.connect
-            return h.view('index', data)
-        }
+          const [result] = await connect.query("SELECT * FROM faqs WHERE deleted=false");
+          return result;
+        },
+        description: "Get All Faqs",
+        notes: "get faqs",
+        tags: ["api"], // ADD THIS TAG
+      },
     },
     {
-        method:'GET',
-        path:'/dynamic',
-        handler:(req, h) => {
-            const data = {
-                name:'wt'
-            }
-            return h.view('index', data)
-        }
-    }
-  ])
+      method: "GET",
+      path: "/faqs/{id}",
+      options: {
+        handler: async (req, h) => {
+          const { id } = req.params;
+          const [result] = await connect.query(
+            `SELECT * FROM faqs WHERE deleted=false AND id=${id}`
+          );
+          return result[0];
+        },
+        description: "Get Faqs by Id",
+        notes: "get faqs",
+        tags: ["api"], // ADD THIS TAG
+        validate: {
+          params: Joi.object({
+            id: Joi.number().required().description("the id"),
+          }),
+        },
+      },
+    },
+    {
+      method: "POST",
+      path: "/faqs",
+      options: {
+        handler: async (req, h) => {
+          console.log(req.payload)
+          
+          /* const [result] = await connect.query(
+            `INSERT INTO faqs () 
+            VALUES ()`
+          );
+          return result; */
+          return "test"
+        },
+        description: "Create Faqs",
+        notes: "get faqs",
+        tags: ["api"], // ADD THIS TAG
+        plugins: {
+          "hapi-swagger": {
+            payloadType: "form",
+          },
+        },
+        validate: {
+          payload: Joi.object({
+            question: Joi.string().required('mandatory'),
+            answer: Joi.string().required('mandatory'),
+            order:Joi.number(),
+            deleted:Joi.boolean(),
+            createdAt:Joi.number(),
+            updatedAt:Joi.number(),
+            createdBy:Joi.number(),
+            updatedBy:Joi.number(),
+            deletedBy:Joi.number(),
+            deletedAt:Joi.number()
+          }),
+        },
+      },
+    },
+    {
+      method: "PUT",
+      path: "/faqs/{id}",
+      options: {
+        handler: async (req, h) => {
+          console.log(req.payload)
+          
+          const [result] = await connect.query(
+            `UPDATE faqs SET`
+          );
+          return "test"
+        },
+        description: "Edit Faqs",
+        notes: "get faqs",
+        tags: ["api"], // ADD THIS TAG
+        plugins: {
+          "hapi-swagger": {
+            payloadType: "form",
+          },
+        },
+        validate: {
+          payload: Joi.object({
+            question: Joi.string().required('mandatory'),
+            answer: Joi.string().required('mandatory'),
+            order:Joi.number(),
+            deleted:Joi.boolean(),
+            createdAt:Joi.number(),
+            updatedAt:Joi.number(),
+            createdBy:Joi.number(),
+            updatedBy:Joi.number(),
+            deletedBy:Joi.number(),
+            deletedAt:Joi.number()
+          }),
+        },
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/faqs/{id}",
+      options: {
+        handler: async (req, h) => {
+          const { id } = req.params;
+          const [result] = await connect.query(
+            `DELETE FROM faqs WHERE id=${id}`
+          );
+          return result;
+        },
+        description: "Delete Faqs",
+        notes: "get faqs",
+        tags: ["api"], // ADD THIS TAG
+        validate: {
+          params: Joi.object({
+            id: Joi.number().required().description("the id"),
+          }),
+        },
+      },
+    },
+  ]);
 
-  await server.start();
-  console.log(`server running on %s`, server.info.uri);
+  try {
+    await server.start();
+    console.log("Server running at:", server.info.uri);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 process.on("unhandledRejection", (err) => {
